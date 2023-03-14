52b820e92086f1a66fb5eb8054481eafafea6d23
evilstorm <gevilstorm@gmail.com>
2023-03-14 14:02:28 +0900

    added email verification state

--- models/model_user.js
+++ models/model_user.js
@@ -5,6 +5,7 @@ const schema = new Schema(
   {
     firebaseId: { type: String, index: true, unique: true },
     email: { type: String, index: true },
+    emailValification: { type: Boolean, default: false },
     joinType: { type: Number, default: 0 }, //0: 회원가입, 1:구글, 2: 카카오, 3: 페이스북, 4: 애플
     secureLevel: { type: Number, default: 0 }, //0: 일반, 6: 관리자, 9: 모든 권한
     setting: { type: Schema.Types.ObjectId, ref: "Setting", default: null },
